9e3e4a676cb1ca6d97a7343835165eb3fde01ee5
Roman Abramov <rabramov@elvees.com>
2023-02-16 19:57:51 +0300

    Restart nginx before launching delved application.

--- vscode/scripts/launch-deploy.sh
+++ vscode/scripts/launch-deploy.sh
@@ -16,10 +16,22 @@ xunreferenced_variables "${WRAPPER_TYPE}"
 SERVICES_STOP=(
 	"onvifd" 
 #	"onvifd-debug"
+	"nginx" 
+)
+
+# List of services to be started
+SERVICES_START=(
+	"nginx"
 )
 
 # List of process names to be stopped
 PROCESSES_STOP=("${TARGET_BIN_SOURCE}" "${TARGET_BIN_NAME}")
+
+# List of directories to be created
+DIRECTORIES_CREATE=(
+	"/tmp/nginx"
+)
+
 
 # List of files to be copied, "source|target"
 COPY_FILES=(
@@ -27,7 +39,12 @@ COPY_FILES=(
 	".vscode/scripts/dlv-stop.sh|:/usr/bin/dstop"
 	"${TARGET_BIN_SOURCE}|:${TARGET_BIN_DESTIN}"
 	"init/onvifd.conf|:/etc/onvifd.conf"
-	)
+	"${BUILDROOT_DIR}/output/target/usr/sbin/nginx|:/usr/sbin/nginx"
+	"init/ipcam.tmpl|:/var/lib/onvifd/ipcam.tmpl"
+	"init/users.digest|:/var/lib/onvifd/users.digest"
+
+
+)
 
 xunreferenced_variables \
 	"${SERVICES_STOP[@]}" \
@@ -40,5 +57,7 @@ xbuild
 xecho "Deploying ${PI}${TARGET_BIN_NAME}${PO} to remote host ${PI}${TARGET_USER}@${TARGET_IPADDR}${PO}"
 xsstop
 xpstop
+xmkdirs
 xfcopy
+xsstart
 xflash
