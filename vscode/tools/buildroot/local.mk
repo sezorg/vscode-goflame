98bf9d969474873c7bf02070c0f817af8718492d
Roman Abramov <rabramov@elvees.com>
2023-06-08 18:30:26 +0300

    Add ecam03 buildroot related scripts & configs.

---
+++ vscode/tools/buildroot/local.mk
@@ -0,0 +1,6 @@
+BASE = $(HOME)/Workspace/Elvees
+#ONVIFD_OVERRIDE_SRCDIR = $(BASE)/activities/onvifd_rel
+#MEDIAD_OVERRIDE_SRCDIR = $(BASE)/buildroots/sources/mediad
+#GST_PLUGINS_CV_OVERRIDE_SRCDIR = $(BASE)/buildroots/sources/gst-plugins-cv
+#IPCAMWEB_FRONTEND_OVERRIDE_SRCDIR = $(BASE)/buildroots/sources/ipcamweb
+
