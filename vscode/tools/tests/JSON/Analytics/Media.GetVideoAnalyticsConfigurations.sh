3ef6b36c1a377a0e4d25a55a84dc7d8e52ce327c
Roman Abramov <rabramov@elvees.com>
2023-10-04 11:15:15 +0300

    Update JSON/SOAP test scripts.

--- vscode/tools/tests/JSON/Analytics/Media.GetVideoAnalyticsConfigurations.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-. "./../../CameraConfiguration.sh"
-
-echo "Media.GetVideoAnalyticsConfigurations()"
-./ipcam_request.py -a 10.113.11.163 -s media -c GetVideoAnalyticsConfigurations -l {} | jq
