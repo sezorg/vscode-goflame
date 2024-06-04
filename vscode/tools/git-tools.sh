54e4de7d05dfd9a3f940cce96d425a8a37b65dd1
Roman Abramov <rabramov@elvees.com>
2024-06-04 10:49:31 +0300

    Add support for TTY console communication over bash.

--- vscode/tools/git-tools.sh
+++ vscode/tools/git-tools.sh
@@ -332,6 +332,44 @@ function _tty_resolve_ip() {
 	done
 }
 
+function tty_sh() {
+	if [[ $# -lt 1 ]]; then
+		echo "Usage:"
+		echo "  $0 <serial-port> [ <speed> [ <stty-options> ... ] ]"
+		echo "  Example: $0 /dev/ttyUSB0 115200"
+		echo "  Press Ctrl+Q to quit"
+	fi
+
+	set -e # Exit when any command fails
+
+	# Save settings of current terminal to restore later
+	original_settings="$(stty -g)"
+
+	# Kill background process and restore terminal when this shell exits
+	trap 'set +e; kill "$bgPid"; stty "$original_settings"' EXIT
+
+	# Remove serial port from parameter list, so only stty settings remain
+	port="$1"
+	shift
+
+	# Set up serial port, append all remaining parameters from command line
+	stty -F "$port" raw -echo "$@"
+
+	# Set current terminal to pass through everything except Ctrl+Q
+	# * "quit undef susp undef" will disable Ctrl+\ and Ctrl+Z handling
+	# * "isig intr ^Q" will make Ctrl+Q send SIGINT to this script
+	stty raw -echo isig intr ^Q quit undef susp undef
+
+	printf "Connecting to %s. Press Ctrl+Q to exit.\n\r# " "$port"
+
+	# Let cat read the serial port to the screen in the background
+	# Capture PID of background process so it is possible to terminate it
+	cat "$port" &
+	bgPid=$!
+
+	cat >"$port" # Redirect all keyboard input to serial port
+}
+
 function ss() {
 	local user="root" pass="root" ip_address
 	ip_address=$(_resolve_variable "$*" "" "last_ip_addr" "Target IP address parameter expected")
@@ -384,7 +422,8 @@ function tt() {
 	TTY_PORT="$device_path"
 	_tty_resolve_port
 	_set_konsole_title "TTY on $TTY_PORT" "TTY on $TTY_PORT"
-	"$SCRIPT_DIR/tty.sh" "$TTY_PORT" "$TTY_SPEED"
+	tty_sh "$TTY_PORT" "$TTY_SPEED"
+	#"$SCRIPT_DIR/tty.sh" "$TTY_PORT" "$TTY_SPEED"
 	_set_konsole_title
 }
 
