96a4fe528148ee95a0ca110837d95c94e9b4d875
Roman Abramov <rabramov@elvees.com>
2024-10-10 14:06:55 +0300

    Add new master branches: mcom03.

--- vscode/tools/git-tools.sh
+++ vscode/tools/git-tools.sh
@@ -46,7 +46,7 @@ function gh() {
 }
 
 function master_branch_lookup() {
-	local branches=("master" "main" "ipcam" "ecam02" "ecam03")
+	local branches=("master" "main" "ipcam" "ecam02" "ecam03" "mcom03")
 	for branch in "${branches[@]}"; do
 		if (git rev-parse --verify "$branch" >/dev/null 2>&1); then
 			echo "$branch"
